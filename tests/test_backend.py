3a38f8c87b55aa522c3cbb81dc5fb86b3be20c60
Maor Grab <maor.grab@gmail.com>
2025-06-09 12:51:41 +0300

    update test

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,16 +1,19 @@
+import pytest
 from fastapi.testclient import TestClient
 from deploy_chatbot_python.backend.server import api, Query
 from deploy_chatbot_python.config import constants
 
 
-def test_get_root():
+@pytest.mark.asyncio
+async def test_get_root():
     with TestClient(api) as client:
         response = client.get("/")
         assert response.status_code == 200
         assert response.json() == {"response": "This is a chatbot"}
 
 
-def test_post_query():
+@pytest.mark.asyncio
+async def test_post_query():
     with TestClient(api) as client:
         response = client.post(
             f"/{constants.API_POST_ENDPOINT}",
